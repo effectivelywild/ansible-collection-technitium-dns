c99fd261a870c3fa334b5f68c5a489ec7a8094a5
Frank Muise <14898486+effectivelywild@users.noreply.github.com>
2025-09-07 10:56:17 -0400

    Add update_nsec3_parameters

--- README.md
+++ README.md
@@ -1,3 +1,107 @@
 # Ansible Collection - technetium.dns
 
+[![CI](https://github.com/effectivelywild/ansible-collection-technitium-dns/workflows/CI/badge.svg?event=push)](https://github.com/effectivelywild/ansible-collection-technitium-dns/actions) [![codecov](https://codecov.io/github/effectivelywild/ansible-collection-technitium-dns/graph/badge.svg?token=UVSWMN1RV1)](https://codecov.io/github/effectivelywild/ansible-collection-technitium-dns)
+
 Ansible collection for Technitium DNS based on the [API](https://github.com/TechnitiumSoftware/DnsServer/blob/master/APIDOCS.md).
+
+
+We follow the [Ansible Code of Conduct](https://docs.ansible.com/ansible/devel/community/code_of_conduct.html) in all our interactions within this project.
+
+If you encounter abusive behavior, please refer to the [policy violations](https://docs.ansible.com/ansible/devel/community/code_of_conduct.html#policy-violations) section of the Code for information on how to raise a complaint.
+
+## Communication
+
+For more information about communication, refer to the [Ansible Communication guide](https://docs.ansible.com/ansible/devel/community/communication.html).
+
+## Contributing to this collection
+
+The content of this collection is made by people like you, a community of individuals collaborating on making the world better through developing automation software.
+
+We are actively accepting new contributors.
+
+Any kind of contribution is very welcome.
+
+You don't know how to start? Refer to our [contribution guide](CONTRIBUTING.md)!
+
+We use the following guidelines:
+
+* [CONTRIBUTING.md](CONTRIBUTING.md)
+* [REVIEW_CHECKLIST.md](REVIEW_CHECKLIST.md)
+* [Ansible Community Guide](https://docs.ansible.com/ansible/latest/community/index.html)
+* [Ansible Development Guide](https://docs.ansible.com/ansible/devel/dev_guide/index.html)
+* [Ansible Collection Development Guide](https://docs.ansible.com/ansible/devel/dev_guide/developing_collections.html#contributing-to-collections)
+
+## Collection maintenance
+
+The current maintainers are listed in the [MAINTAINERS](MAINTAINERS) file. If you have questions or need help, feel free to mention them in the proposals.
+
+To learn how to maintain / become a maintainer of this collection, refer to the [Maintainer guidelines](MAINTAINING.md).
+
+## Governance
+
+The process of decision making in this collection is based on discussing and finding consensus among participants.
+
+Every voice is important. If you have something on your mind, create an issue or dedicated discussion and let's discuss it!
+
+
+## Ansible version compatibility
+
+TBD
+
+## Python version compatibility
+
+TBD
+
+## Included content
+See the complete list of collection content in the [Plugin Index](TBD).
+
+### Installing the Collection from Ansible Galaxy
+
+Before using this collection, you need to install it with the Ansible Galaxy command-line tool:
+```bash
+ansible-galaxy collection install effectivelywild.technitium_dns
+```
+
+You can also include it in a `requirements.yml` file and install it with `ansible-galaxy collection install -r requirements.yml`, using the format:
+```yaml
+---
+collections:
+  - name: effectivelywild.technitium_dns
+```
+
+Note that if you install the collection from Ansible Galaxy, it will not be upgraded automatically when you upgrade the `ansible` package. To upgrade the collection to the latest available version, run the following command:
+```bash
+ansible-galaxy collection install effectivelywild.technitium_dns --upgrade
+```
+
+You can also install a specific version of the collection, for example, if you need to downgrade when something is broken in the latest version (please report an issue in this repository). Use the following syntax to install version `0.1.0`:
+
+```bash
+ansible-galaxy collection install effectivelywild.technitium_dns:==0.1.0
+```
+
+See [Ansible Using collections](https://docs.ansible.com/ansible/devel/user_guide/collections_using.html) for more details.
+
+## Using this collection
+
+TBD
+
+## Release notes
+
+See the [changelog](https://github.com/effectivelywild/ansible-collection-technitium-dns/tree/main/CHANGELOG.rst).
+
+## More information
+
+- [Ansible Collection overview](https://github.com/ansible-collections/overview)
+- [Ansible User guide](https://docs.ansible.com/ansible/devel/user_guide/index.html)
+- [Ansible Developer guide](https://docs.ansible.com/ansible/devel/dev_guide/index.html)
+- [Ansible Collections Checklist](https://github.com/ansible-collections/overview/blob/main/collection_requirements.rst)
+- [Ansible Community code of conduct](https://docs.ansible.com/ansible/devel/community/code_of_conduct.html)
+- [The Bullhorn (the Ansible Contributor newsletter)](https://us19.campaign-archive.com/home/?u=56d874e027110e35dea0e03c1&id=d6635f5420)
+- [News for Maintainers](https://github.com/ansible-collections/news-for-maintainers)
+
+## Licensing
+
+GNU General Public License v3.0 or later.
+
+See [LICENSE](https://www.gnu.org/licenses/gpl-3.0.txt) to see the full text.
---
+++ plugins/modules/technitium_dns_update_nsec3_parameters.py
@@ -0,0 +1,191 @@
+#!/usr/bin/python
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+DOCUMENTATION = r'''
+---
+module: technitium_dns_update_nsec3_parameters
+short_description: Update NSEC3 Parameters
+version_added: "0.2.0"
+author: Frank Muise (@effectivelywild)
+description:
+  - Updates the iteration and salt length parameters for NSEC3 on a signed DNS zone.
+  - The zone must already be signed with DNSSEC and using NSEC3.
+seealso:
+  - module: effectivelywild.technitium_dns.technitium_dns_sign_zone
+    description: Sign a zone with DNSSEC
+  - module: effectivelywild.technitium_dns.technitium_dns_convert_to_nsec3
+    description: Convert signed zone from NSEC to NSEC3
+  - module: effectivelywild.technitium_dns.technitium_dns_get_dnssec_properties
+    description: Get DNSSEC properties for a zone
+options:
+  api_port:
+    description:
+      - Port for the Technitium DNS API. Defaults to 5380
+    required: false
+    type: int
+    default: 5380
+  api_token:
+    description:
+      - API token for authentication
+    required: true
+    type: str
+  api_url:
+    description:
+      - Base URL for the Technitium DNS API
+    required: true
+    type: str
+  validate_certs:
+    description:
+      - Whether to validate SSL certificates when making API requests.
+    required: false
+    type: bool
+    default: true
+  zone:
+    description:
+      - The name of the primary zone to update NSEC3 parameters for
+    required: true
+    type: str
+  iterations:
+    description:
+      - The number of iterations to use for hashing
+      - Default value is 0 when not specified
+    required: false
+    type: int
+    default: 0
+  salt_length:
+    description:
+      - The length of salt in bytes to use for hashing
+      - Default value is 0 when not specified
+    required: false
+    type: int
+    default: 0
+'''
+
+EXAMPLES = r'''
+- name: Update NSEC3 parameters with custom iterations and salt length
+  technitium_dns_update_nsec3_parameters:
+    api_url: "http://localhost"
+    api_token: "myapitoken"
+    zone: "example.com"
+    iterations: 10
+    salt_length: 8
+
+- name: Reset NSEC3 parameters to defaults
+  technitium_dns_update_nsec3_parameters:
+    api_url: "http://localhost"
+    api_token: "myapitoken"
+    zone: "example.com"
+    iterations: 0
+    salt_length: 0
+
+- name: Update only iterations parameter
+  technitium_dns_update_nsec3_parameters:
+    api_url: "http://localhost"
+    api_token: "myapitoken"
+    zone: "example.com"
+    iterations: 5
+'''
+
+RETURN = r'''
+changed:
+    description: Whether the module made changes
+    type: bool
+    returned: always
+    sample: true
+failed:
+    description: Whether the module failed
+    type: bool
+    returned: always
+    sample: false
+msg:
+    description: Human readable message describing the result
+    type: str
+    returned: always
+    sample: "NSEC3 parameters updated for zone 'example.com'"
+api_response:
+    description: Full API response from Technitium DNS server
+    type: dict
+    returned: always
+    sample: {
+        "status": "ok"
+    }
+'''
+
+from ansible_collections.effectivelywild.technitium_dns.plugins.module_utils.technitium import TechnitiumModule
+
+
+class UpdateNSEC3ParametersModule(TechnitiumModule):
+    argument_spec = dict(
+        **TechnitiumModule.get_common_argument_spec(),
+        zone=dict(type='str', required=True),
+        iterations=dict(type='int', required=False, default=0),
+        salt_length=dict(type='int', required=False, default=0),
+    )
+
+    module_kwargs = dict(
+        supports_check_mode=True
+    )
+
+    def run(self):
+        zone = self.params['zone']
+        iterations = self.params['iterations']
+        salt_length = self.params['salt_length']
+
+        # Get DNSSEC properties - this includes status and current NSEC3 parameters
+        dnssec_props = self.get_dnssec_properties(zone)
+        dnssec_status = dnssec_props.get('dnssecStatus', '').lower()
+
+        if dnssec_status == 'unsigned':
+            self.fail_json(msg=f"Zone '{zone}' is not signed. NSEC3 parameters can only be updated on signed zones.")
+
+        # Check if zone is using NSEC3 by looking for NSEC3 parameters
+        if 'nsec3Iterations' not in dnssec_props:
+            self.fail_json(msg=f"Zone '{zone}' is not using NSEC3. Convert to NSEC3 first.")
+
+        # Check if parameters are already set as requested
+        current_iterations = dnssec_props.get('nsec3Iterations', 0)
+        current_salt_length = dnssec_props.get('nsec3SaltLength', 0)
+
+        params_changed = not (current_iterations == iterations and current_salt_length == salt_length)
+
+        if not params_changed:
+            self.exit_json(
+                changed=False,
+                msg=f"NSEC3 parameters for zone '{zone}' are already set to iterations={iterations}, salt_length={salt_length}",
+                api_response={'status': 'ok', 'msg': 'No changes needed'}
+            )
+
+        if self.check_mode:
+            self.exit_json(
+                changed=True,
+                msg=f"NSEC3 parameters for zone '{zone}' would be updated to iterations={iterations}, salt_length={salt_length} (check mode)",
+                api_response={}
+            )
+
+        # Update NSEC3 parameters
+        query = {
+            'zone': zone,
+            'iterations': iterations,
+            'saltLength': salt_length
+        }
+
+        data = self.request('/api/zones/dnssec/properties/updateNSEC3Params', params=query, method='POST')
+        status = data.get('status')
+        error_msg = data.get('errorMessage') or data.get('error') or data.get('message') or "Unknown error"
+
+        if status != 'ok':
+            self.fail_json(msg=f"Technitium API error: {error_msg}", api_response=data)
+
+        self.exit_json(
+            changed=True,
+            msg=f"NSEC3 parameters updated for zone '{zone}' (iterations={iterations}, salt_length={salt_length})",
+            api_response=data
+        )
+
+
+if __name__ == '__main__':
+    module = UpdateNSEC3ParametersModule()
+    module.run()
